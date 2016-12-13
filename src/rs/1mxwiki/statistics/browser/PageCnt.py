fd0134c284f5c144b6c6f9a1d0c31ff5603bbc39
RealWaynebird <ferdinand.Sedlmair@gmx.net>
2016-12-13 12:32:03 +0100

    Update PageCnt.py

--- src/rs/1mxwiki/statistics/browser/PageCnt.py
+++ src/rs/1mxwiki/statistics/browser/PageCnt.py
@@ -2,9 +2,12 @@ from plone import api
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 import logging
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import alsoProvides
 
 class PageCnt(BrowserView):
-	def pagecnt(self):
+	def pagecnt(self,request):
+		alsoProvides(request, IDisableCSRFProtection)
  		catalog = getToolByName(self.context, 'portal_catalog')
  		results = catalog.searchResults({'portal_type': 'Document'})
  		logger = logging.getLogger('statistics')		
