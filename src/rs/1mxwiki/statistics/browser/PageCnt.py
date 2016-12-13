da7ddf718b24c40b93cc7c75b6bc73659c2fc321
RealWaynebird <ferdinand.Sedlmair@gmx.net>
2016-12-13 10:58:05 +0100

    Update PageCnt.py

--- src/rs/1mxwiki/statistics/browser/PageCnt.py
+++ src/rs/1mxwiki/statistics/browser/PageCnt.py
@@ -5,9 +5,13 @@ import logging
 
 class PageCnt(BrowserView):
 	def pagecnt(self):
-		context = self.context
-		catalog = getToolByName(self.context, 'portal_catalog')
-		results = catalog.searchResults({'portal_type': 'Document'})
-		logging.basicConfig(filename='/opt/plone/zeocluster/var/zeoserver/statistics.log', filemode='w', level=logging.INFO)
-		logging.info('%(asctime)s %(levelname)s' + str(len(results)) + " Pages in the Plone")
+ 		catalog = getToolByName(self.context, 'portal_catalog')
+ 		results = catalog.searchResults({'portal_type': 'Document'})
+ 		logger = logging.getLogger('statistics')		
+		lhdlr = logging.FileHandler('/opt/plone/zeocluster/var/zeoserver/statistics.log', filemode='w')
+ 		formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+ 		lhdlr.setFormatter(formatter)
+  		logger.addHandler(lhdlr)
+		logger.info(str(len(results)) + " Pages in the Plone")
+
 
