87750a3f7b66af099fcd7b883a0013d989c2db68
RealWaynebird <ferdinand.Sedlmair@gmx.net>
2016-12-14 15:20:39 +0100

    Update PageCnt.py

--- src/rs/1mxwiki/statistics/browser/PageCnt.py
+++ src/rs/1mxwiki/statistics/browser/PageCnt.py
@@ -2,16 +2,13 @@ from plone import api
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 import logging
-from plone.protect.interfaces import IDisableCSRFProtection
-from zope.interface import alsoProvides
 
 class PageCnt(BrowserView):
-	def pagecnt(self, context, request):
-		alsoProvides(request, IDisableCSRFProtection)
+	def pagecnt(self):
  		catalog = getToolByName(self.context, 'portal_catalog')
  		results = catalog.searchResults({'portal_type': 'Document'})
  		logger = logging.getLogger('statistics')		
-		lhdlr = logging.FileHandler('/opt/plone/zeocluster/var/zeoserver/statistics.log', mode='w')
+		lhdlr = logging.FileHandler('/opt/plone/zeocluster/var/zeoserver/statistics.log', 'w')
  		formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
  		lhdlr.setFormatter(formatter)
   		logger.addHandler(lhdlr)
