4e68aeeca1b195f53b137772c1efa2c9075fc81f
RealWaynebird <ferdinand.Sedlmair@gmx.net>
2017-01-03 16:25:09 +0100

    Create ShowLog.py

---
+++ src/rs/1mxwiki/statistics/browser/ShowLog.py
@@ -0,0 +1,10 @@
+from plone import api
+from Products.Five import BrowserView
+
+class ShowLog(BrowserView):
+        def __init__(self,context,request):
+                return
+         
+        def __call__(self):
+                with open('/opt/plone/zeocluster/var/zeoserver/statistics.log') as log
+                        return log.read()
